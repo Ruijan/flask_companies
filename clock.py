97ebb89576291e3ede794faf7d49ce3dcc212b88
Julien Rechenmann <rechenmann@gmail.com>
2020-03-23 15:30:15 +0100

    reduce clock period to 1min

--- clock.py
+++ clock.py
@@ -12,7 +12,7 @@ def timed_job():
     print('This job is run every three minutes.')
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minutes=15)
+@sched.scheduled_job('cron', day_of_week='mon-fri', minutes=1)
 def scheduled_job():
     print('This job is run every weekday every 15min.')
     client = pymongo.MongoClient(os.environ["MONGO_URI"])
@@ -21,7 +21,7 @@ def scheduled_job():
     companies = pd.DataFrame.from_records(collection.find())
     companies = companies.sample(frac=1).reset_index(drop=True)
     logs = ""
-    process_companies(companies, db, logs, 15 * 60)
+    process_companies(companies, db, logs, 30)
     client.close()
 
 
