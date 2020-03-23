698a2cef8f8f351ca9ebc2abb7a07f407a5a5691
Julien Rechenmann <rechenmann@gmail.com>
2020-03-23 16:21:55 +0100

    remove cron job

--- clock.py
+++ clock.py
@@ -12,17 +12,17 @@ def timed_job():
     print('This job is run every three minutes.')
 
 
-@sched.scheduled_job('cron', minutes=1)
-def scheduled_job():
-    print('This job is run every day every 15min.')
-    client = pymongo.MongoClient(os.environ["MONGO_URI"])
-    db = client.finance
-    collection = db.cleaned_companies
-    companies = pd.DataFrame.from_records(collection.find())
-    companies = companies.sample(frac=1).reset_index(drop=True)
-    logs = ""
-    process_companies(companies, db, logs, 30)
-    client.close()
+# @sched.scheduled_job('cron', minutes=1)
+# def scheduled_job():
+#     print('This job is run every day every 15min.')
+#     client = pymongo.MongoClient(os.environ["MONGO_URI"])
+#     db = client.finance
+#     collection = db.cleaned_companies
+#     companies = pd.DataFrame.from_records(collection.find())
+#     companies = companies.sample(frac=1).reset_index(drop=True)
+#     logs = ""
+#     process_companies(companies, db, logs, 30)
+#     client.close()
 
 
 sched.start()
