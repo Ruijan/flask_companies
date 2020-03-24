8b9e32f3543ea16a6e46bd6c8ced900915d46262
Julien Rechenmann <rechenmann@gmail.com>
2020-03-24 14:22:38 +0100

    check with one time job

--- clock.py
+++ clock.py
@@ -10,6 +10,14 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('This job is run every three minutes.')
+    client = pymongo.MongoClient(os.environ["MONGO_URI"])
+    db = client.finance
+    collection = db.cleaned_companies
+    companies = pd.DataFrame.from_records(collection.find())
+    companies = companies.sample(frac=1).reset_index(drop=True)
+    logs = ""
+    process_companies(companies, db, logs, 30)
+    client.close()
 
 
 # @sched.scheduled_job('cron', minutes=1)
