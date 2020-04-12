678134d1210acbd4127666ddccc27cb925d9b5a6
Julien Rechenmann <rechenmann@gmail.com>
2020-04-12 12:23:16 +0200

    less time precised tests

--- tests/cache/test_local_history_cache.py
+++ tests/cache/test_local_history_cache.py
@@ -9,19 +9,19 @@ class MyTestCase(unittest.TestCase):
         today = datetime.today()
         (end_date, start_date) = get_range(None, '1d', None)
         self.assertEqual(today, end_date)
-        self.assertEqual(today - timedelta(days=1), start_date)
+        self.assertLessEqual((today - timedelta(days=1) - start_date).seconds, 1, 'Expect start_date to be 1 day ago')
 
     def test_get_five_days_range(self):
         today = datetime.today()
         (end_date, start_date) = get_range(None, '5d', None)
         self.assertEqual(today, end_date)
-        self.assertEqual(today - timedelta(days=5), start_date)
+        self.assertLessEqual((today - timedelta(days=5) - start_date).seconds, 1, 'Expect start_date to be 5 days ago')
 
     def test_get_five_weeks_range(self):
         today = datetime.today()
         (end_date, start_date) = get_range(None, '5w', None)
         self.assertEqual(today, end_date)
-        self.assertEqual(today - timedelta(weeks=5), start_date)
+        self.assertLessEqual((today - timedelta(weeks=5) - start_date).seconds, 1, 'Expect start_date to be 5 weeks ago')
 
 
 if __name__ == '__main__':
