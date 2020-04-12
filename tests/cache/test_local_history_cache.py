acf80351eb2097b2b7918108a6ff4ed763ef420a
Julien Rechenmann <rechenmann@gmail.com>
2020-04-12 15:38:50 +0200

    add hours to other part

--- tests/cache/test_local_history_cache.py
+++ tests/cache/test_local_history_cache.py
@@ -11,7 +11,7 @@ class MyTestCase(unittest.TestCase):
         (end_date, start_date) = get_range(None, '1d', None)
         self.assertLessEqual((today - end_date).seconds, 1,
                              'Expect end_date to be today. Received: ' + end_date.strftime(
-                                 "%Y-%m-%d") + " compared to " + today.strftime("%Y-%m-%d %H:%M:%S"))
+                                 "%Y-%m-%d %H:%M:%S") + " compared to " + today.strftime("%Y-%m-%d %H:%M:%S"))
         self.assertLessEqual((today - timedelta(days=1) - start_date).seconds, 1, 'Expect start_date to be 1 day ago')
 
     def test_get_five_days_range(self):
@@ -19,7 +19,7 @@ class MyTestCase(unittest.TestCase):
         (end_date, start_date) = get_range(None, '5d', None)
         self.assertLessEqual((today - end_date).seconds, 1,
                              'Expect end_date to be today. Received: ' + end_date.strftime(
-                                 "%Y-%m-%d") + " compared to " + today.strftime("%Y-%m-%d %H:%M:%S"))
+                                 "%Y-%m-%d %H:%M:%S") + " compared to " + today.strftime("%Y-%m-%d %H:%M:%S"))
         self.assertLessEqual((today - timedelta(days=5) - start_date).seconds, 1, 'Expect start_date to be 5 days ago')
 
     def test_get_five_weeks_range(self):
@@ -27,7 +27,7 @@ class MyTestCase(unittest.TestCase):
         (end_date, start_date) = get_range(None, '5w', None)
         self.assertLessEqual((today - end_date).seconds, 1,
                              'Expect end_date to be today. Received: ' + end_date.strftime(
-                                 "%Y-%m-%d") + " compared to " + today.strftime("%Y-%m-%d %H:%M:%S"))
+                                 "%Y-%m-%d %H:%M:%S") + " compared to " + today.strftime("%Y-%m-%d %H:%M:%S"))
         self.assertLessEqual((today - timedelta(weeks=5) - start_date).seconds, 1,
                              'Expect start_date to be 5 weeks ago')
 
