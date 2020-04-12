f57cad691ef7e319571180dfb65f61f5251404e1
Julien Rechenmann <rechenmann@gmail.com>
2020-04-12 15:29:43 +0200

    give more details

--- tests/cache/test_local_history_cache.py
+++ tests/cache/test_local_history_cache.py
@@ -9,20 +9,27 @@ class MyTestCase(unittest.TestCase):
     def test_get_one_day_range(self):
         today = datetime.today()
         (end_date, start_date) = get_range(None, '1d', None)
-        self.assertLessEqual((today - end_date).seconds, 1, 'Expect end_date to be today. Received: ' + end_date.strftime("%Y-%m-%d"))
+        self.assertLessEqual((today - end_date).seconds, 1,
+                             'Expect end_date to be today. Received: ' + end_date.strftime(
+                                 "%Y-%m-%d") + " compared to " + today.strftime("%Y-%m-%d"))
         self.assertLessEqual((today - timedelta(days=1) - start_date).seconds, 1, 'Expect start_date to be 1 day ago')
 
     def test_get_five_days_range(self):
         today = datetime.today()
         (end_date, start_date) = get_range(None, '5d', None)
-        self.assertLessEqual((today - end_date).seconds, 1, 'Expect end_date to be today. Received: ' + end_date.strftime("%Y-%m-%d"))
+        self.assertLessEqual((today - end_date).seconds, 1,
+                             'Expect end_date to be today. Received: ' + end_date.strftime(
+                                 "%Y-%m-%d") + " compared to " + today.strftime("%Y-%m-%d"))
         self.assertLessEqual((today - timedelta(days=5) - start_date).seconds, 1, 'Expect start_date to be 5 days ago')
 
     def test_get_five_weeks_range(self):
         today = datetime.today()
         (end_date, start_date) = get_range(None, '5w', None)
-        self.assertLessEqual((today - end_date).seconds, 1, 'Expect end_date to be today. Received: ' + end_date.strftime("%Y-%m-%d"))
-        self.assertLessEqual((today - timedelta(weeks=5) - start_date).seconds, 1, 'Expect start_date to be 5 weeks ago')
+        self.assertLessEqual((today - end_date).seconds, 1,
+                             'Expect end_date to be today. Received: ' + end_date.strftime(
+                                 "%Y-%m-%d") + " compared to " + today.strftime("%Y-%m-%d"))
+        self.assertLessEqual((today - timedelta(weeks=5) - start_date).seconds, 1,
+                             'Expect start_date to be 5 weeks ago')
 
 
 if __name__ == '__main__':
